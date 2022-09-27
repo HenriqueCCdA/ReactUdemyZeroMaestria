83917724c5ffcc27222b13714554c2fcb9a35480
Henrique Andrade <henrique.ccda@gmail.com>
2022-09-26 23:24:37 -0300

    m

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponets.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponets.js
@@ -1,7 +1,13 @@
+// arquivos de estivo
+
 const FirstComponent = () => {
+  // essa função faz isso
+
   return (
     <div>
+      {/* Algum comentário */ }
       <h1>Meu primeiro componente</h1>
+      <p className='teste'>Meu texto</p>
     </div>
   )
 }
