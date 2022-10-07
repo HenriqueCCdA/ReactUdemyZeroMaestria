94862769e579fa764e64fc0a8722b16b8fecadc6
Henrique Andrade <henrique.ccda@gmail.com>
2022-10-07 02:42:43 -0300

    m

--- 3_AVANCANDO_EM_REACT/avancando_em_react/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando_em_react/src/App.js
@@ -7,9 +7,23 @@ import ConditinalRender from './components/ConditinalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
 import ShowUsername from './components/ShowUsername';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
 
 function App() {
   const [userName] = useState("Maria");
+
+  const cars = [
+    {id: 1, brand: 'Ferrari', color: 'Amarela', newCar: true, km: 0},
+    {id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 34343},
+    {id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 243},
+  ];
+
+  function showMessage() {
+    console.log('Evento do componente');
+  }
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -32,6 +46,27 @@ function App() {
       <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true}/>
       {/* reaproveitando */}
       <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false}/>
+      {/* loop rm array de objetos */}
+      {cars.map((car) => (
+        <CarDetails
+          key={car.id}
+          brand={car.brand}
+          color={car.brand}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* fragment */}
+      <Fragment propFragment="teste"/>
+      {/* children */}
+      <Container myValue="testing">
+        <p>Este é o conteudo</p>
+      </Container>
+      <Container myValue="testing 2">
+        <h5>Testando o container</h5>
+      </Container>
+      {/* Executar funcção */}
+      <ExecuteFunction myFunction={showMessage}/>
     </div>
   );
 }
